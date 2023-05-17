6c6c92f63ebfa8d8c63038f2dfef84e7431c0929
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-18 01:24:52 +0600

    done the another home page section

--- src/components/pages/HomePage/Home.jsx
+++ src/components/pages/HomePage/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BannerSlider from './BannerSlider';
 import ToyGallery from './ToyGallery';
+import WhyWeAreBest from './WhyWeAreBest';
 
 const Home = () => {
     return (
         <div>
             <BannerSlider />
             <ToyGallery />
+            <WhyWeAreBest />
         </div>
     );
 };
---
+++ src/components/pages/HomePage/WhyWeAreBest.jsx
@@ -0,0 +1,96 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable max-len */
+import React from 'react';
+
+const WhyWeAreBest = () => {
+    return (
+        <div>
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 pb-14 mx-auto">
+                    <div data-aos="fade-up" className="text-center mb-20">
+                        <h1 className="sm:text-3xl text-2xl font-bold title-font text-primary mb-4">Why We Are Best In The Market</h1>
+                        <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
+                            Toy-Troppers Baby toy shop used to be known as the A to Z Factory and has been in the body business for over 25 years. Currently we have 12 outlets in major city in USA,
+                            where customer easily take their ordered product <span className="text-xl font-bold tracking-wider text-orange-600">without delivery charge.</span>
+                        </p>
+                        <div className="flex mt-6 justify-center">
+                            <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex" />
+                        </div>
+                    </div>
+                    <div data-aos="zoom-in-up" className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+                        <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+                            <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 24 24">
+                                    <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                                </svg>
+                            </div>
+                            <div className="flex-grow">
+                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Lowest Prices Guaranteed</h2>
+                                <p className="leading-relaxed text-base">
+                                    Toy-Troppers price match applies only to USA internet retailers that have the identical product and for goods IN STOCK available.
+                                </p>
+                                <a href="#" className="mt-3 text-indigo-500 inline-flex items-center">
+                                    Learn More
+                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                        <path d="M5 12h14M12 5l7 7-7 7" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+                            <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 24 24">
+                                    <circle cx="6" cy="6" r="3" />
+                                    <circle cx="6" cy="18" r="3" />
+                                    <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
+                                </svg>
+                            </div>
+                            <div className="flex-grow">
+                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Rearrange Delivery if Changes Ming</h2>
+                                <p className="leading-relaxed text-base">Everything we sell online is covered under distance selling regulations by a 14-day-coding of period.</p>
+                                <a href="#" className="mt-3 text-indigo-500 inline-flex items-center">
+                                    Learn More
+                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                        <path d="M5 12h14M12 5l7 7-7 7" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+                            <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 24 24">
+                                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+                                    <circle cx="12" cy="7" r="4" />
+                                </svg>
+                            </div>
+                            <div className="flex-grow">
+                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Established 25 years Ago</h2>
+                                <p className="leading-relaxed text-base">In 1996, We made our Final move to our current premises in the heart of Newyork. we are located in a point OutDoor Market.</p>
+                                <a href="#" className="mt-3 text-indigo-500 inline-flex items-center">
+                                    Learn More
+                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                        <path d="M5 12h14M12 5l7 7-7 7" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <a
+                        data-aos="fade-up"
+                        href="https://www.google.com/maps?q=New+York+City"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        type="button"
+                        className="flex w-72 mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                        Visit Our Office Location
+                    </a>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default WhyWeAreBest;
