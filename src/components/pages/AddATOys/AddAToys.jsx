8eea7182b4cff8f185acdf78d69c74749e655a62
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-18 21:12:53 +0600

    add toys full functional now create a backend for stroing the data

--- src/components/pages/AddATOys/AddAToys.jsx
+++ src/components/pages/AddATOys/AddAToys.jsx
@@ -28,74 +28,81 @@ export default function AddToyPage() {
     };
 
     return (
-        <div className="container mx-auto py-8 grid grid-cols-3">
-            <form onSubmit={handleSubmit(onSubmit)} className="col-span-2 max-w-2xl gap-5 grid grid-cols-2 w-full mx-auto">
-                <div className="mb-4">
-                    <label htmlFor="pictureUrl" className="block font-medium mb-2">
-                        Picture URL of the toy
-                    </label>
-                    <input type="text" id="pictureUrl" {...register('pictureUrl')} className="input input-bordered w-full" required />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="name" className="block font-medium mb-2">
-                        Name
-                    </label>
-                    <input type="text" id="name" {...register('name')} className="input input-bordered w-full" required />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="sellerName" className="block font-medium mb-2">
-                        Seller Name
-                    </label>
-                    <input type="text" id="sellerName" {...register('sellerName')} className="input input-bordered w-full" />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="sellerEmail" className="block font-medium mb-2">
-                        Seller Email
-                    </label>
-                    <input type="email" id="sellerEmail" {...register('sellerEmail')} className="input input-bordered w-full" />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="subCategory" className="block font-medium mb-2">
-                        Sub-category
-                    </label>
-                    <select id="subCategory" {...register('subCategory')} className="input input-bordered w-full">
-                        <option value="Marvel">Marvel</option>
-                        <option value="Star Wars">Star Wars</option>
-                        <option value="Transformers">Transformers</option>
-                    </select>
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="price" className="block font-medium mb-2">
-                        Price
-                    </label>
-                    <input type="number" id="price" {...register('price')} className="input input-bordered w-full" required />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="rating" className="block font-medium mb-2">
-                        Rating
-                    </label>
-                    <input type="number" id="rating" {...register('rating')} className="input input-bordered w-full" required />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="quantity" className="block font-medium mb-2">
-                        Available Quantity
-                    </label>
-                    <input type="number" id="quantity" {...register('quantity')} className="input input-bordered w-full" required />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="description" className="block font-medium mb-2">
-                        Detail Description
-                    </label>
-                    <textarea id="description" {...register('description')} className="input input-bordered w-full" rows={10} />
-                </div>
-                <div className="mt-8">
-                    <button type="submit" className="btn btn-block btn-primary">
-                        Add Toy
-                    </button>
-                </div>
-            </form>
+        <>
+            <h1 data-aos="fade-up" className="text-4xl font-bold tracking-widest text-center py-6 text-primary">
+                Add a New Toy
+            </h1>
+            <div className="container mx-auto py-8 grid grid-cols-3">
+                <form data-aos="fade-left" onSubmit={handleSubmit(onSubmit)} className="col-span-2 max-w-2xl gap-5 grid grid-cols-2 w-full mx-auto">
+                    <div className="mb-4">
+                        <label htmlFor="pictureUrl" className="block font-medium mb-2">
+                            Picture URL of the toy
+                        </label>
+                        <input type="text" id="pictureUrl" {...register('pictureUrl')} className="input input-bordered input-primary w-full" required />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="name" className="block font-medium mb-2">
+                            Name
+                        </label>
+                        <input type="text" id="name" {...register('name')} className="input input-bordered input-primary w-full" required />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="sellerName" className="block font-medium mb-2">
+                            Seller Name
+                        </label>
+                        <input type="text" id="sellerName" {...register('sellerName')} className="input input-bordered input-primary w-full" />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="sellerEmail" className="block font-medium mb-2">
+                            Seller Email
+                        </label>
+                        <input type="email" id="sellerEmail" {...register('sellerEmail')} className="input input-bordered input-primary w-full" />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="subCategory" className="block font-medium mb-2">
+                            Sub-category
+                        </label>
+                        <select id="subCategory" {...register('subCategory')} className="input input-bordered input-primary w-full">
+                            <option value="Marvel">Marvel</option>
+                            <option value="Star Wars">Star Wars</option>
+                            <option value="Transformers">Transformers</option>
+                        </select>
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="price" className="block font-medium mb-2">
+                            Price
+                        </label>
+                        <input type="number" id="price" {...register('price')} className="input input-bordered input-primary w-full" required />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="rating" className="block font-medium mb-2">
+                            Rating
+                        </label>
+                        <input type="number" id="rating" {...register('rating')} className="input input-bordered input-primary w-full" required />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="quantity" className="block font-medium mb-2">
+                            Available Quantity
+                        </label>
+                        <input type="number" id="quantity" {...register('quantity')} className="input input-bordered input-primary w-full" required />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="description" className="block font-medium mb-2">
+                            Detail Description
+                        </label>
+                        <textarea id="description" {...register('description')} className="input input-bordered input-primary w-full" rows={10} />
+                    </div>
+                    <div className="mt-8">
+                        <button type="submit" className="btn btn-block btn-primary">
+                            Add Toy
+                        </button>
+                    </div>
+                </form>
 
-            <Lottie options={defaultOptions} height={600} width={500} />
-        </div>
+                <div data-aos="fade-right">
+                    <Lottie options={defaultOptions} height={600} width={500} />
+                </div>
+            </div>
+        </>
     );
 }
