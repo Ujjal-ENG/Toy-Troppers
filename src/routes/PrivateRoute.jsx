06066ecf7efe07ad4535ee373ff3776857a3d8ac
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-19 13:25:14 +0600

    set animated loading bar

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable react/jsx-indent */
 import React, { useContext } from 'react';
+import Lottie from 'react-lottie';
 import { Navigate, useLocation } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import animationData from '../assets/json/loading.json';
 import { AuthContext } from '../context/AuthProvider';
 
 function PrivateRoute({ children }) {
     const { userInfo, privateLoad } = useContext(AuthContext);
     const location = useLocation();
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
     if (location.pathname.includes('details') && !userInfo) {
         Swal.fire({
             icon: 'error',
@@ -19,7 +30,8 @@ function PrivateRoute({ children }) {
     if (privateLoad) {
         return (
             <div className="h-screen flex justify-center items-center">
-                <progress className="progress w-56" />
+                {/* <progress className="progress w-56" /> */}
+                <Lottie options={defaultOptions} height={700} width={600} />
             </div>
         );
     }
