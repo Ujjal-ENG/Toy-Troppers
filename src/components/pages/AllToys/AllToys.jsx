459be14ef0e7bd7e40b121b015e64a2b6134b489
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-20 01:43:51 +0600

    error set the all toys page

--- src/components/pages/AllToys/AllToys.jsx
+++ src/components/pages/AllToys/AllToys.jsx
@@ -53,68 +53,74 @@ const AllToys = () => {
 
     return (
         <div className="py-10">
-            <div className="flex justify-center md:justify-end pb-8">
-                <div className="form-control w-80">
-                    <span className="footer-title text-primary">Search the Product Using the Toy Name</span>
+            {isError && <span className="text-red-500">Error occurred while fetching data.</span>}
+            {toys.length > 0 && (
+                <>
+                    {' '}
+                    <div className="flex justify-center md:justify-end pb-8">
+                        <div className="form-control w-80">
+                            <span className="footer-title text-primary">Search the Product Using the Toy Name</span>
 
-                    <div className="relative">
-                        <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Iron Man" className="input input-bordered w-full pr-16" />
-                        <button type="button" className="btn btn-primary absolute top-0 right-0 rounded-l-none">
-                            Search Here
-                        </button>
+                            <div className="relative">
+                                <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Iron Man" className="input input-bordered w-full pr-16" />
+                                <button type="button" className="btn btn-primary absolute top-0 right-0 rounded-l-none">
+                                    Search Here
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className="overflow-x-auto">
-                <table className="table table-compact w-full text-center">
-                    <thead>
-                        <tr>
-                            <th />
-                            <th>Seller Name</th>
-                            <th>Seller Email</th>
-                            <th>Toy Name</th>
-                            <th>Sub Category</th>
-                            <th>Price</th>
-                            <th>Available Quantity</th>
-                            <th />
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {toys &&
-                            toys.map((toy, idx) => (
-                                <tr key={toy._id}>
-                                    <th>{idx + 1}</th>
-                                    <td>{toy.sellerName || 'Not Available'}</td>
-                                    <td>{toy.sellerEmail || 'Not Available'}</td>
-                                    <td>{toy.name || 'Not Available'}</td>
-                                    <td>{toy.subCategory || 'Not Available'}</td>
-                                    <td>${toy.price || 0}</td>
-                                    <td>{toy.availableQuantity || 0}</td>
-                                    <td>
-                                        <Link to={`toy-details/${toy._id}`} className="btn btn-primary  inline-flex gap-3 btn-sm text-sm font-bold items-center mt-3">
-                                            View Details
-                                            <FiArrowRight className="text-xl font-bold" />
-                                        </Link>
-                                    </td>
+                    <div className="overflow-x-auto">
+                        <table className="table table-compact w-full text-center">
+                            <thead>
+                                <tr>
+                                    <th />
+                                    <th>Seller Name</th>
+                                    <th>Seller Email</th>
+                                    <th>Toy Name</th>
+                                    <th>Sub Category</th>
+                                    <th>Price</th>
+                                    <th>Available Quantity</th>
+                                    <th />
                                 </tr>
+                            </thead>
+                            <tbody>
+                                {toys &&
+                                    toys.map((toy, idx) => (
+                                        <tr key={toy._id}>
+                                            <th>{idx + 1}</th>
+                                            <td>{toy.sellerName || 'Not Available'}</td>
+                                            <td>{toy.sellerEmail || 'Not Available'}</td>
+                                            <td>{toy.name || 'Not Available'}</td>
+                                            <td>{toy.subCategory || 'Not Available'}</td>
+                                            <td>${toy.price || 0}</td>
+                                            <td>{toy.availableQuantity || 0}</td>
+                                            <td>
+                                                <Link to={`toy-details/${toy._id}`} className="btn btn-primary  inline-flex gap-3 btn-sm text-sm font-bold items-center mt-3">
+                                                    View Details
+                                                    <FiArrowRight className="text-xl font-bold" />
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    ))}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="flex justify-center items-center gap-2">
+                        <div className="btn-group my-5 ">
+                            {pageNumbers.map((number) => (
+                                <button key={number} type="button" className={`btn ${number === currentPage ? 'btn-active' : ''}`} onClick={() => setCurrentPage(number)}>
+                                    {number + 1}
+                                </button>
                             ))}
-                    </tbody>
-                </table>
-            </div>
-            <div className="flex justify-center items-center gap-2">
-                <div className="btn-group my-5 ">
-                    {pageNumbers.map((number) => (
-                        <button key={number} type="button" className={`btn ${number === currentPage ? 'btn-active' : ''}`} onClick={() => setCurrentPage(number)}>
-                            {number + 1}
-                        </button>
-                    ))}
-                </div>
-                <select value={itemsPerPage} onChange={handleOptionChange} className="select select-bordered w-32">
-                    {options.map((option) => (
-                        <option key={option}>{option}</option>
-                    ))}
-                </select>
-            </div>
+                        </div>
+                        <select value={itemsPerPage} onChange={handleOptionChange} className="select select-bordered w-32">
+                            {options.map((option) => (
+                                <option key={option}>{option}</option>
+                            ))}
+                        </select>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
