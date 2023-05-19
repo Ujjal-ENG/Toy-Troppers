4b6b0ae7ab87d68da3d1f43d05da5e07ed34add2
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-19 19:52:22 +0600

    done the footer

--- src/components/layouts/shared/Footer.jsx
+++ src/components/layouts/shared/Footer.jsx
@@ -1,7 +1,256 @@
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/jsx-indent-props */
 import React from 'react';
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <div>
+            <footer className="text-white body-font bg-black">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap md:text-left text-center -mb-10 -mx-4">
+                        <div className="lg:w-1/6 md:w-1/2 w-full px-4">
+                            <h2 className="title-font font-bold text-white tracking-widest text-xl mb-3">About Us</h2>
+                            <nav className="list-none mb-10">
+                                <li>
+                                    <a href="https://www.sideshow.com/about?ref=footer_our-story" target="_blank" className="text-white cursor-pointer hover:text-gray-400" rel="noreferrer">
+                                        Our Story
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.shareasale.com/shareasale.cfm?merchantID=73004" target="_blank" className="text-white cursor-pointer hover:text-gray-400" rel="noreferrer">
+                                        Affiliate Program
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.sideshow.com/about/wholesale/?ref=footer_wholesale-program"
+                                        target="_blank"
+                                        className="text-white cursor-pointer hover:text-gray-400"
+                                        rel="noreferrer">
+                                        Wholesale Program
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.sideshow.com/contact?ref=footer_press-inquiries" target="_blank" className="text-white cursor-pointer hover:text-gray-400" rel="noreferrer">
+                                        Press Inquiries
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.sideshow.com/careers?ref=footer_careers" target="_blank" className="text-white cursor-pointer hover:text-gray-400" rel="noreferrer">
+                                        Career
+                                    </a>
+                                </li>
+                            </nav>
+                        </div>
+                        <div className="lg:w-1/6 md:w-1/2 w-full px-4">
+                            <h2 className="title-font font-bold text-white tracking-widest text-xl mb-3">Customer Rewards</h2>
+                            <nav className="list-none mb-10">
+                                <li>
+                                    <a href="https://www.sideshow.com/code" target="_blank" className="text-white cursor-pointer hover:text-gray-400" rel="noreferrer">
+                                        Promo Code Lookup
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.sideshow.com/code" target="_blank" className="text-white cursor-pointer hover:text-gray-400" rel="noreferrer">
+                                        Gift Card Lookup
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://help.sideshow.com/hc/en-us/articles/207381828-Sideshow-Rewards-Program"
+                                        target="_blank"
+                                        className="text-white cursor-pointer hover:text-gray-400"
+                                        rel="noreferrer">
+                                        Earn Rewards
+                                    </a>
+                                </li>
+                            </nav>
+                        </div>
+                        <div className="lg:w-1/6 md:w-1/2 w-full px-4">
+                            <h2 className="title-font font-bold text-white tracking-widest text-xl mb-3">Customer Support</h2>
+                            <nav className="list-none mb-10">
+                                <li>
+                                    <a
+                                        href="https://help.sideshow.com/hc/en-us/categories/201551668-Returns-Exchanges"
+                                        target="_blank"
+                                        className="text-white cursor-pointer hover:text-gray-400"
+                                        rel="noreferrer">
+                                        Returns & Exchange
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://help.sideshow.com/hc/en-us/categories/200319108-Shipping-Tracking"
+                                        target="_blank"
+                                        className="text-white cursor-pointer hover:text-gray-400"
+                                        rel="noreferrer">
+                                        Shipping Information
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://help.sideshow.com/hc/en-us/articles/206615657-Track-Your-Order-"
+                                        target="_blank"
+                                        className="text-white cursor-pointer hover:text-gray-400"
+                                        rel="noreferrer">
+                                        Track Your Order
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.sideshow.com/why-sideshow/flexible-payment-plans?ref=footer_payment-plans"
+                                        target="_blank"
+                                        className="text-white cursor-pointer hover:text-gray-400"
+                                        rel="noreferrer">
+                                        Payment Rules
+                                    </a>
+                                </li>
+                            </nav>
+                        </div>
+                        <div className="lg:w-1/6 md:w-1/2 w-full px-4">
+                            <h2 className="title-font font-bold text-white tracking-widest text-xl mb-3">Services</h2>
+                            <nav className="list-none mb-10">
+                                <li>
+                                    <a href="https://business.adobe.com/blog/basics/how-to-build-a-brand" target="_blank" className="text-white cursor-pointer hover:text-gray-400" rel="noreferrer">
+                                        Branding
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://proximityschool.com/learn/design-process/" target="_blank" className="text-white cursor-pointer hover:text-gray-400" rel="noreferrer">
+                                        Design
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.smallbusiness.wa.gov.au/marketing/8-steps-to-marketing"
+                                        target="_blank"
+                                        className="text-white cursor-pointer hover:text-gray-400"
+                                        rel="noreferrer">
+                                        Marketing
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.facebook.com/business/ads?content_id=kosuUE8pXDLhEfZ&ref=sem_smb&utm_term=dsa-1627345090764&gclid=CjwKCAjwvJyjBhApEiwAWz2nLavahIRZSaf43qUgJ7Bz2CBa5WCXBQAHUsOKxL5JAUqQVe2J0EPhTxoC9m0QAvD_BwE"
+                                        target="_blank"
+                                        className="text-white cursor-pointer hover:text-gray-400"
+                                        rel="noreferrer">
+                                        Advertisment
+                                    </a>
+                                </li>
+                            </nav>
+                        </div>
+                        <div className="lg:w-1/6 md:w-1/2 w-full px-4">
+                            <h2 className="title-font font-bold text-white tracking-widest text-xl mb-3">Legal</h2>
+                            <nav className="list-none mb-10">
+                                <li>
+                                    <a href="https://www.sideshow.com/terms?ref=footer_terms-conditions" target="_blank" className="text-white cursor-pointer hover:text-gray-400" rel="noreferrer">
+                                        Terms of use
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.sideshow.com/privacy-policy?ref=footer_privacy-policy"
+                                        target="_blank"
+                                        className="text-white cursor-pointer hover:text-gray-400"
+                                        rel="noreferrer">
+                                        Privacy policy
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.sideshow.com/accessibility-statement?ref=footer_accessibility-statement"
+                                        target="_blank"
+                                        className="text-white cursor-pointer hover:text-gray-400"
+                                        rel="noreferrer">
+                                        Accessibility Statement
+                                    </a>
+                                </li>
+                            </nav>
+                        </div>
+                        <div className="lg:w-1/6 md:w-1/2 w-full px-4">
+                            <h2 className="title-font font-bold text-white tracking-widest text-xl mb-3">Retail Store Locator</h2>
+                            <nav className="list-none mb-10">
+                                <li>
+                                    <a href="https://www.google.com/maps?q=USA" target="_blank" className="text-white  cursor-pointer hover:text-gray-400" rel="noreferrer">
+                                        Click here for the location
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/maps?q=USA" target="_blank" className="text-white cursor-pointer hover:text-gray-400" rel="noreferrer">
+                                        Help Center
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/maps?q=USA" target="_blank" className="text-white cursor-pointer hover:text-gray-400" rel="noreferrer">
+                                        Contact Us
+                                    </a>
+                                </li>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div className="border-t border-gray-200">
+                    <div className="container px-5 py-8 flex flex-wrap mx-auto items-center">
+                        <div className="flex md:flex-nowrap flex-wrap justify-center items-end md:justify-start">
+                            <div className="relative sm:w-64 w-40 sm:mr-4 mr-2">
+                                <label htmlFor="footer-field" className="leading-7 text-sm text-white">
+                                    NEWSLETTER
+                                </label>
+                                <div className="form-control w-80">
+                                    <label className="label">
+                                        <span className="label-text text-white">Enter your email address</span>
+                                    </label>
+                                    <div className="relative">
+                                        <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
+                                        <button type="button" className="btn btn-primary absolute top-0 right-0 rounded-l-none">
+                                            Subscribe
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <span className="inline-flex lg:ml-auto lg:mt-0 mt-6 w-full justify-center md:justify-start md:w-auto">
+                            <a href="https://www.facebook.com/ujjal.roy.7862" target="_blank" className="text-gray-500 cursor-pointer hover:text-primary" rel="noreferrer">
+                                <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+                                </svg>
+                            </a>
+                            <a href="https://www.facebook.com/ujjal.roy.7862" target="_blank" className="ml-3 text-gray-500 hover:text-sky-700" rel="noreferrer">
+                                <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+                                </svg>
+                            </a>
+                            <a href="https://www.linkedin.com/in/ujjal-kumar-roy/" target="_blank" rel="noreferrer" className="ml-3 text-gray-500 hover:text-red-500">
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                                </svg>
+                            </a>
+                            <a href="https://www.linkedin.com/in/ujjal-kumar-roy/" target="_blank" className="ml-3 text-gray-500  hover:text-sky-700" rel="noreferrer">
+                                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+                                    <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" />
+                                    <circle cx="4" cy="4" r="2" stroke="none" />
+                                </svg>
+                            </a>
+                        </span>
+                    </div>
+                </div>
+                <div className="bg-gray-700">
+                    <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+                        <p className="text-gray-500 text-sm text-center sm:text-left">
+                            © 2023 Toy | Troppers —
+                            <a href="https://twitter.com/knyttneve" className="text-white ml-1" target="_blank" rel="noopener noreferrer">
+                                @Ujjal Kumar Roy
+                            </a>
+                        </p>
+                        <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">Thanks for visiting this websites!!</span>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    );
 };
 
 export default Footer;
